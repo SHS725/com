73c53464d66cbe1800065852e36d88b3edd9080c
SHS725 <68070637+SHS725@users.noreply.github.com>
2021-05-17 18:10:43 +0900

    Add files via upload

---
+++ ms.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <pwd.h>
+
+#define MAX_LEN_LINE    100
+#define LEN_HOSTNAME	30
+
+int main(void)
+{
+    char command[MAX_LEN_LINE];
+    char *args[] = {command, NULL};
+    int ret, status;
+    pid_t pid, cpid;
+
+    char hostname[LEN_HOSTNAME + 1];
+    memset(hostname, 0x00, sizeof(hostname));
+    gethostname(hostname, LEN_HOSTNAME);
+
+    while (true) {
+        char *s;
+        int len;
+        
+        printf("\x1b[33mSHS's SHELL \n");
+        printf("\x1b[36musername: %s\n", getpwuid(getuid())->pw_name);
+        printf("\x1b[36mhostname: %s\n",	 hostname);
+
+        s = fgets(command, MAX_LEN_LINE, stdin);
+        if (s == NULL) {
+            fprintf(stderr, "fgets failed\n");
+            exit(1);
+        }
+        
+        len = strlen(command);
+        printf("%d\n", len);
+        if (command[len - 1] == '\n') {
+            command[len - 1] = '\0'; 
+        }
+        
+        printf("[%s]\n", command);
+      
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "fork failed\n");
+            exit(1);
+        } 
+        if (pid != 0) {  /* parent */
+            cpid = waitpid(pid, &status, 0);
+            if (cpid != pid) {
+                fprintf(stderr, "waitpid failed\n");        
+            }
+            printf("Child process terminated\n");
+            if (WIFEXITED(status)) {
+                printf("Exit status is %d\n", WEXITSTATUS(status)); 
+            }
+        }
+        else {  /* child */
+            ret = execve(args[0], args, NULL);
+            if (ret < 0) {
+                fprintf(stderr, "execve failed\n");   
+                return 1;
+            }
+        } 
+    }
+    return 0;
+}
